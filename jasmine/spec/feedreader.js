15515d796f4517a5ca561d61dbc0b376439e4122
Ash <ash.duckett@outlook.com>
2017-02-13 22:17:39 +0000

    Added some array size checking.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,46 +27,159 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /* This test ensures that all feed items have
+           a url property defined on them and that they
+           each contain a value */
+         it('all have a url property', function() {
+            allFeeds.forEach(function(feed) {
+                // Ensure it's been defined
+                expect(feed.url).toBeDefined();
 
+                // A non-empty string will be truthy
+                expect(feed.url).toBeTruthy();
+            });
+         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        /* This test ensures that all feed items have
+           a url property defined on them and that they
+           each contain a value */
+        it('all have a name property', function() {
+            allFeeds.forEach(function(feed) {
+                // Ensure it's been defined
+                expect(feed.name).toBeDefined();
+
+                // A non-empty string will be truthy
+                expect(feed.name).toBeTruthy();
+            });
+         });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    describe('The Menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* This test tests to see if the slide menu is
+           hidden by default.
          */
+        it('should initially hidden', function() {
+            var body = document.body;
+            expect(body.classList.contains('menu-hidden')).toBeTruthy();
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+         /* This test ensures that when the menu icon is clicked, from
+            its initial state, the menu is shown, then on clicking it again
+            it's hidden once more.
           */
+        it('should toggle visibility on clicking its menu icon', function() {
+            var menuIcon = document.getElementsByClassName('menu-icon-link')[0];
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+            // First verify that the menu is hidden
+            var bodyClassList = document.body.classList;
+            expect(bodyClassList.contains('menu-hidden')).toBeTruthy();
 
+            // next click the menu's icon and verify that it's showing
+            menuIcon.click();
+            expect(bodyClassList.contains('menu-hidden')).toBeFalsy();
+
+            // Click it once more to see that it should be hidden again
+            menuIcon.click();
+            expect(bodyClassList.contains('menu-hidden')).toBeTruthy();
+
+        });
+    });
+
+    describe('Initial Entries', function() {
+   
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        
+        beforeEach(function(done) {
+            // Clear out the feeds or we won't be testing the case where the loadFeeds()
+            // method brings nothing back
+            var feedContainer = document.getElementsByClassName('feed')[0];
+            if(feedContainer) {
+                feedContainer.innerHTML = "";
+            }
+            
+            // Run the async function
+            loadFeed(0, function() {
+                done();
+            });
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        // Tests that the loadFeed() function actually displays some results
+        it('should have at least one entry in the .feeds container after loading', function(done) {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+            // Ensure there is at least 1 feed element
+            var entryLinks = document.querySelector('.feed > .entry-link');
+            
+            // If nothing is found, this will be false since null is truthy
+            expect(entryLinks).toBeTruthy();
+            done();
+
+        });
+
+    describe('New Feed Selection', function() {
+        
+        var initialFeedItem = null;
+        var afterLoadFeedItem = null;
+
+        beforeEach(function(done) {
+            // Clear out the feeds or we won't be testing the case where the loadFeeds()
+            // method brings nothing back
+            
+            // Run the async function
+            if(allFeeds && allFeeds instanceof Array && allFeeds.length >= 0) {
+                loadFeed(0, function() {
+                    // Grab the first item's text after an initial load
+                    initialFeedItem = document.querySelector('.feed > .entry-link h2').textContent;
+                    done();
+                });
+            } else {
+                console.log('allFeeds is either empty or not set. Could not complete test.');
+            }
+        });
+
+         beforeEach(function (done) {
+            if(allFeeds && allFeeds instanceof Array && allFeeds.length >= 1) {
+                loadFeed(1, function() {
+                    // Grab the first item's text after some new data's been loaded
+                    afterLoadFeedItem = document.querySelector('.feed > .entry-link h2').textContent;
+                    done();
+                });
+            } else {
+                console.log('allFeeds is either empty, has less than one element or is not set at all. Could not complete test.');
+            }
+
+        });
+
+
+
+        it('should display new content on new feed selection', function(done) {
+            // Once the beforeEach() has run, we have the initial value of the first header
+            // Now we can generate a new batch
+            
+            // The following four tests just ensure that we have meaningful data and aren't
+            // just comparing an initial state with an aftermath state of null, in which
+            // case they wouldn't be equal, but the test would pass.
+
+            // If these exist they will either be strings or empty strings
+            expect(initialFeedItem).toBeDefined();
+            expect(afterLoadFeedItem).toBeDefined();
+
+            // If they are empty strings, they'll be falsy
+            expect(initialFeedItem).toBeTruthy();
+            expect(afterLoadFeedItem).toBeTruthy();
+          
+            // Ensure new content has been loaded
+            expect(initialFeedItem).not.toEqual(afterLoadFeedItem);
+            done();
+        });
+
+    });
+  
+    });
 }());
